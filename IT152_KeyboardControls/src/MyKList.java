2c37c45ea43cfc01f36e9271ae3db8eadcf18482
John Christian Ventura <130374378+legilimensjohn@users.noreply.github.com>
2024-05-09 22:52:38 +0800

    Update MyKList.java

--- IT152_KeyboardControls/src/MyKList.java
+++ IT152_KeyboardControls/src/MyKList.java
@@ -6,6 +6,7 @@ import javax.swing.JComponent;
 class MyKList implements KeyListener {
 	
 	private JComponent[] letLabels;
+	
 	//Contructor that accepts an array comprising JComponents elements
 	public MyKList (JComponent[] letLabels) {
 		this.letLabels = letLabels;
@@ -20,11 +21,20 @@ class MyKList implements KeyListener {
 	@Override
 	//This method is invoked when a key is pressed down
 	public void keyPressed(KeyEvent e) {	
+		
 		// TODO Auto-generated method stub
-		char keyChar = Character.toUpperCase(e.getKeyChar());	//Transform the key character to uppercase
-		if (e.getKeyCode() >= KeyEvent.VK_A && e.getKeyCode() <= KeyEvent.VK_Z) {	//Verify if the pressed key falls within the range of A-Z
-			int index = e.getKeyCode() - KeyEvent.VK_A;		//Compute the index by considering the key's position in the alphabet
-			letLabels[index].setBackground(Color.ORANGE);	//Change the the background color of the corresponding JComponent to a refreshing shade of orange
+		//Transform the key character to uppercase
+		char keyChar = Character.toUpperCase(e.getKeyChar());	
+
+		//Verify if the pressed key falls within the range of A-Z
+		if (e.getKeyCode() >= KeyEvent.VK_A && e.getKeyCode() <= KeyEvent.VK_Z) {	
+
+			//Compute the index by considering the key's position in the alphabet
+			int index = e.getKeyCode() - KeyEvent.VK_A;		
+
+			//Change the the background color of the corresponding JComponent to a refreshing shade of orange
+			letLabels[index].setBackground(Color.ORANGE);	
+			
 		}
 		
 	}
@@ -32,11 +42,19 @@ class MyKList implements KeyListener {
 	@Override
 	//This method is invoked when a key is released
 	public void keyReleased(KeyEvent e) {
+		
 		// TODO Auto-generated method stub
-		char keyChar = Character.toUpperCase(e.getKeyChar());	//Transform the key character to uppercase
-		if (e.getKeyCode() >= KeyEvent.VK_A && e.getKeyCode() <= KeyEvent.VK_Z) {	//Verify if the pressed key falls within the range of A-Z
-			int index = e.getKeyCode() - KeyEvent.VK_A;		//Compute the index by considering the key's position in the alphabet
-			letLabels[index].setBackground(Color.CYAN);		//Alter the background color of the corresponding JComponent to a pristine cyan hue
+		//Transform the key character to uppercase
+		char keyChar = Character.toUpperCase(e.getKeyChar());	
+
+		//Verify if the pressed key falls within the range of A-Z
+		if (e.getKeyCode() >= KeyEvent.VK_A && e.getKeyCode() <= KeyEvent.VK_Z) {	
+			
+			//Compute the index by considering the key's position in the alphabet
+			int index = e.getKeyCode() - KeyEvent.VK_A;		
+			
+			//Alter the background color of the corresponding JComponent to a pristine cyan hue
+			letLabels[index].setBackground(Color.CYAN);		
 		}
 	}
 
