b39bf881162f2cdc568b5f04598b58e5989036ba
John Christian Ventura <130374378+legilimensjohn@users.noreply.github.com>
2024-05-10 22:21:54 +0800

    Update KeyControls.java

--- IT152_KeyboardControls/src/KeyControls.java
+++ IT152_KeyboardControls/src/KeyControls.java
@@ -13,32 +13,57 @@ public class KeyControls extends JFrame {
 	private JLabel[] letLabels;
 	
 	//Instantiate the KeyControls class by calling its contructor, which extends JFrame
-	public KeyControls() {			
-		super("Keyboard Controls");		//Assign a title to the JFrame
-		p = new JPanel (new GridLayout(2,14));	//Generate a JPanel that will hold JLabels in a grid layout
+	public KeyControls() {		
+
+		//Assign a title to the JFrame
+		super("Keyboard Controls");		
+
+		//Generate a JPanel that will hold JLabels in a grid layout
+		p = new JPanel (new GridLayout(2,14));	
+		
 		p.setBackground(Color.ORANGE);
-		add(p);							//Append the JPanel to the JFrame
+
+		//Append the JPanel to the JFrame
+		add(p);							
 		
 		//Declare an array of JLabels that will represent letters from A to Z
 		letLabels = new JLabel[26];		
 		for (char c = 'A'; c <= 'Z'; c++) {
-			letLabels [c - 'A'] = new JLabel(String.valueOf(c));	//Construct a label that will display the letter
-			letLabels [c - 'A'].setOpaque(true);		//Display the label's background color
-			letLabels [c - 'A'].setBackground(Color.CYAN);	//Assign the background color of the label to cyan
-			letLabels [c - 'A'].setHorizontalAlignment(JLabel.CENTER);		//Horizontally center the text of the label
-			letLabels [c - 'A'].setVerticalAlignment(JLabel.CENTER);		//Vertically center the text of the label
-			letLabels [c - 'A'].setFont(letLabels [c - 'A'].getFont().deriveFont(30.0f));	//Adjust the size of the font
-			p.add(letLabels [c - 'A']);		//Place the the label onto the JPanel
+
+			//Construct a label that will display the letter
+			letLabels [c - 'A'] = new JLabel(String.valueOf(c));	
+			
+			//Display the label's background color
+			letLabels [c - 'A'].setOpaque(true);		
+			
+			//Assign the background color of the label to cyan
+			letLabels [c - 'A'].setBackground(Color.CYAN);	
+
+			//Horizontally center the text of the label
+			letLabels [c - 'A'].setHorizontalAlignment(JLabel.CENTER);	
+
+			//Vertically center the text of the label
+			letLabels [c - 'A'].setVerticalAlignment(JLabel.CENTER);		
+
+			//Adjust the size of the font
+			letLabels [c - 'A'].setFont(letLabels [c - 'A'].getFont().deriveFont(30.0f));	
+
+			//Place the the label onto the JPanel
+			p.add(letLabels [c - 'A']);		
 		}
 		
 		//Enable the JPanel to receive keyboard events by making it focusable
 		p.setFocusable(true);		
+		
 		//Attach a customized KeyListener to the JPanel
 		p.addKeyListener(new MyKList(letLabels));	
+		
 		//Specify the dimensions of the JFrame
 		setSize(500,500);	
+		
 		//Define the action to be taken when the window close button is pressed		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		
+		
 		//Ensure that the JFrame is visible
 		setVisible(true);				
 	}
